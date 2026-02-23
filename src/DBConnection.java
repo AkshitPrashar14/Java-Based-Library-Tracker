46a164ea42c7e340c842a25f7c6e9f9b93760aad
Akshit Prashar <akshitprashar14@gmail.com>
2026-02-23 11:14:07 +0530

    Refactor: Externalized MySQL credentials using environment variables

--- src/DBConnection.java
+++ src/DBConnection.java
@@ -1,3 +1,26 @@
+// import java.sql.Connection;
+// import java.sql.DriverManager;
+
+// public class DBConnection {
+
+//     public static Connection getConnection() {
+//         try {
+//             String url = "jdbc:mysql://localhost:3306/library_db";
+//             String user = "root";
+//             String password = "Abhi@165"; // change if different
+
+//             Connection con = DriverManager.getConnection(url, user, password);
+//             System.out.println("Connected to MySQL successfully!");
+//             return con;
+
+//         } catch (Exception e) {
+//             System.out.println("Database connection failed!");
+//             e.printStackTrace();
+//             return null;
+//         }
+//     }
+// }
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 
@@ -6,8 +29,8 @@ public class DBConnection {
     public static Connection getConnection() {
         try {
             String url = "jdbc:mysql://localhost:3306/library_db";
-            String user = "root";
-            String password = "Abhi@165"; // change if different
+            String user = System.getenv("DB_USER");
+            String password = System.getenv("DB_PASSWORD");
 
             Connection con = DriverManager.getConnection(url, user, password);
             System.out.println("Connected to MySQL successfully!");
